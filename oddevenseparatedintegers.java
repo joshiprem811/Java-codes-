e54e8c9daf5e0be17060b52e0e3bb34276e34712
joshiprem811 <joshiprem811@gmail.com>
2025-05-14 09:35:53 +0530

    Add files via upload

--- oddevenseparatedintegers.java
+++ oddevenseparatedintegers.java
@@ -1,39 +1,42 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class oddevenseparatedintegers {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    public static void separateEvenOdd(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-
-        int[] even = new int[n];
-        int[] odd = new int[n];
-        int evenCount = 0, oddCount = 0;
-
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            if (arr[i] % 2 == 0) {
-                even[evenCount++] = arr[i];
+        while (left < right) {
+            if (arr[left] % 2 == 0) {
+                left++;
+            } else if (arr[right] % 2 != 0) {
+                right--;
             } else {
-                odd[oddCount++] = arr[i];
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
             }
         }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < evenCount; i++) {
-            System.out.print(even[i] + " ");
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-
-        for (int i = 0; i < oddCount; i++) {
-            System.out.print(odd[i] + " ");
-        }
-
-        System.out.println();
-        sc.close();
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        separateEvenOdd(arr);
+        System.out.println("Separated Array: " + Arrays.toString(arr));
+        scanner.close();
     }
 }
